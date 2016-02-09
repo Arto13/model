62a15aacc982c9e14f960e7d7ce480b87932dd53
Artur Petrosyan <a.petrosyan@live.nl>
2016-02-09 23:01:58 +0100

    huiswerk

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+Artist.destroy_all
+afrojack = Artist.create ({name: "Afrojack", age: '28'})
+eminem = Artist.create ({name: "Eminem", age: '43'})
+
+Song.destroy_all
+spark = Song.create ({name: "The Spark"})
+unstoppable = Song.create ({name: "Unstopable"})
+phenomenal = Song.create ({name: "Phenomenal"})
+stan = Song.create ({name: "Stan"})
